e4721fa079a7378b9fa3df2a65b315a0be62b89d
Garth Trickett <garthtrickett@gmail.com>
2020-02-20 09:04:48 +0000

    altered code so binance lob data goes in

--- 95-FI2010/fi_core.py
+++ 95-FI2010/fi_core.py
@@ -1,11 +1,12 @@
 import sys, os
+
 ROOT = os.path.abspath(__file__)
 # Specify the directory depth with respect to the root of your project here
 # (The project root usually holds your data folder and has a depth of 0)
 DIR_DEPTH = 1
 for _ in range(DIR_DEPTH + 1):
-  ROOT = os.path.dirname(ROOT)
-  sys.path.insert(0, ROOT)
+    ROOT = os.path.dirname(ROOT)
+    sys.path.insert(0, ROOT)
 import numpy as np
 import tensorflow as tf
 from tframe import console, SaveMode
@@ -20,8 +21,8 @@ from_root = lambda path: os.path.join(ROOT, path)
 # Initialize config and set data/job dir
 # -----------------------------------------------------------------------------
 th = Config(as_global=True)
-th.data_dir = from_root('95-FI2010/data/')
-th.job_dir = from_root('95-FI2010')
+th.data_dir = from_root("95-FI2010/data/")
+th.job_dir = from_root("95-FI2010")
 # -----------------------------------------------------------------------------
 # Device configurations
 # -----------------------------------------------------------------------------
@@ -33,7 +34,7 @@ th.gpu_memory_fraction = 0.3
 # -----------------------------------------------------------------------------
 th.max_level = 10
 th.volume_only = True
-th.developer_code = 'use_log'
+th.developer_code = "use_log"
 
 th.input_shape = [20]
 th.num_classes = 3
@@ -43,7 +44,7 @@ th.target_dtype = tf.int32
 
 th.val_size = 1
 
-th.loss_string = 'cross_entropy'
+th.loss_string = "cross_entropy"
 
 # -----------------------------------------------------------------------------
 # Set common trainer configs
@@ -63,7 +64,7 @@ th.patience = 10
 th.lives = 1
 
 th.clip_threshold = 1.0
-th.clip_method = 'value'
+th.clip_method = "value"
 
 th.print_cycle = 50
 th.val_progress_bar = True
@@ -81,32 +82,39 @@ th.sub_seq_len = 5000
 th.use_conveyor = True
 th.conveyor_length = 15
 
+
 def activate():
-  # Load datasets
-  train_set, val_set, test_set = du.load_data(th.data_dir)
-  # Calculate class weights
-  if th.class_weights is None and th.loss_string == 'wce':
-    train_targets = train_set.stack.targets.flatten()
-    samples_per_class = [sum(train_targets == c) for c in range(th.num_classes)]
-    class_weights = min(samples_per_class) / np.array(samples_per_class)
-    th.class_weights = class_weights
-    console.show_status(
-      'Class weights set to {}'.format(th.class_weights), '++')
+    # Load datasets
+    train_set, val_set, test_set = du.load_data(th.data_dir)
+    # Calculate class weights
+    if th.class_weights is None and th.loss_string == "wce":
+        train_targets = train_set.stack.targets.flatten()
+        samples_per_class = [sum(train_targets == c) for c in range(th.num_classes)]
+        class_weights = min(samples_per_class) / np.array(samples_per_class)
+        th.class_weights = class_weights
+        console.show_status("Class weights set to {}".format(th.class_weights), "++")
 
-  # Set input shape according to th.max_level and th.volume_only
-  du.FI2010.set_input_shape()
+    # Set input shape according to th.max_level and th.volume_only
+    du.FI2010.set_input_shape()
 
-  # Build model
-  assert callable(th.model)
-  model = th.model(th)
-  assert isinstance(model, Classifier)
+    # Build model
+    assert callable(th.model)
+    model = th.model(th)
+    assert isinstance(model, Classifier)
 
-  # Train or evaluate
-  if th.train: model.train(
-    train_set, validation_set=val_set, test_set=test_set, trainer_hub=th,
-    evaluate=lambda t: du.FI2010.evaluate(t, test_set))
-  else: du.FI2010.evaluate(model, test_set)
 
-  # End
-  model.shutdown()
-  console.end()
+
+    # Train or evaluate
+    if th.train:
+        model.train(
+            train_set,
+            validation_set=val_set,
+            test_set=test_set,
+            trainer_hub=th,
+            evaluate=lambda t: du.FI2010.evaluate(t, test_set),
+        )
+    else:
+        du.FI2010.evaluate(model, test_set)
+    # End
+    model.shutdown()
+    console.end()
